a8926e393baae3a97c6947f7b13b4650843a488e
Pratiksha1501199 <122699860+Pratiksha1501199@users.noreply.github.com>
2026-06-15 10:51:27 +0530

    Merge branch 'fe-PM' of https://github.com/Pratiksha1501199/TPThaneRepo into fe-PM

--- TP_ThanePractice/src/test/java/SampleModulePack/Pratiksha.java
+++ TP_ThanePractice/src/test/java/SampleModulePack/Pratiksha.java
@@ -1,12 +1,24 @@
 package SampleModulePack;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Pratiksha {
 
 	public static void main(String[] args) {
+		
+		 WebDriver driver = new ChromeDriver();
+	        driver.manage().window().maximize();
+
+	        driver.get("https://www.prokabaddi.com");
+	        
 		System.out.println("hello");
-		System.out.println("hey");
-		System.out.println("TL update");
-		System.out.println("TL update2");
+		System.out.println("hi");
+		
+		
+		
+
+
 
 	}
 
