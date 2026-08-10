3ad7bf7a173d3a161c0f346c3d044b3a60576dc8
vmanasaveena19@gmail.com <vmanasaveena19@gmail.com>
2026-08-10 17:48:26 +0530

    completed

--- TP_ThanePractice/src/test/java/SampleModulePack/class1.java
+++ TP_ThanePractice/src/test/java/SampleModulePack/class1.java
@@ -3,6 +3,7 @@ package SampleModulePack;
 public class class1 {
 	public static void main(String[] args) {
 		System.out.println("this is feature");
+		System.out.println("Completed the task");
 	}
 
 }
