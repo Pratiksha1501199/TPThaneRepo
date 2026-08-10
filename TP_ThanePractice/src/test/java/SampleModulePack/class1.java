e660f0778c32dc47509a3cfe2e4bbfa5defcead3
Afzaul Haque <Afzaul Haque@TYSS-Afzaul-Haque>
2026-08-10 17:41:31 +0530

    commit1

--- TP_ThanePractice/src/test/java/SampleModulePack/class1.java
+++ TP_ThanePractice/src/test/java/SampleModulePack/class1.java
@@ -3,6 +3,8 @@ package SampleModulePack;
 public class class1 {
 	public static void main(String[] args) {
 		System.out.println("this is feature");
+		System.out.println("this is TL update");
+
 	}
 
 }
